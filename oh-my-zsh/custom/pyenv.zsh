7f27751a54d74b8828591b63611878dd316cbaa8
Guo,Li <lig@dxc.com>
2025-02-21 11:35:17 +0800

    Updated pyenv config for omz

--- oh-my-zsh/custom/pyenv.zsh
+++ oh-my-zsh/custom/pyenv.zsh
@@ -1,9 +1,4 @@
-# pyenv
-if type pyenv >& /dev/null; then
-    eval "$(pyenv init -)"
-fi
-
-# pyenv-virtualenv
-if type pyenv >& /dev/null; then
-    eval "$(pyenv virtualenv-init -)"
-fi
+export PYENV_ROOT="$HOME/.pyenv"
+[[ -d $PYENV_ROOT/bin ]] && export PATH="$PYENV_ROOT/bin:$PATH"
+eval "$(pyenv init - zsh)"
+eval "$(pyenv virtualenv-init -)"
