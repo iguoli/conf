9be329ee23b0c639fdcf2dfc0fb82649c1713d43
Guo,Li <lig@dxc.com>
2025-02-26 09:18:36 +0800

    Aliased vim to nvim

--- oh-my-zsh/custom/aliases.zsh
+++ oh-my-zsh/custom/aliases.zsh
@@ -5,3 +5,4 @@ alias tree="tree -C"
 alias goproxy="export http_proxy=http://127.0.0.1:7890 https_proxy=http://127.0.0.1:7890 all_proxy=socks5://127.0.0.1:7891"
 alias noproxy="unset all_proxy http_proxy https_proxy"
 
+type nvim >/dev/null 2>&1 && alias vim="nvim"
