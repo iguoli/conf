5cb19f62be049589a8d559a717a5a4edd597dd12
Guo,Li <lig@dxc.com>
2025-03-03 11:20:03 +0800

    Fixed the space issue in alias for WSL env

--- oh-my-zsh/custom/wsl.zsh
+++ oh-my-zsh/custom/wsl.zsh
@@ -1,4 +1,4 @@
-alias code='/mnt/c/Users/lguo5/AppData/Local/Programs/Microsoft VS Code/bin/code'
+alias code='/mnt/c/Users/lguo5/AppData/Local/Programs/Microsoft\ VS\ Code/bin/code'
 alias edge='/mnt/c/Program Files (x86)/Microsoft/Edge/Application/msedge.exe'
-alias bcomp='/mnt/c/Program Files/Beyond Compare 4/BComp.exe'
+alias bcomp='/mnt/c/Program Files/Beyond\ Compare\ 4/BComp.exe'
 alias open='/mnt/c/Windows/explorer.exe'
