0cc3762ff00611fdd640a65da7f0c60ed0a360a8
Guo, Li <28505129+iguoli@users.noreply.github.com>
2025-03-11 21:50:14 +0800

    Updated proxy settings

--- oh-my-zsh/custom/aliases.zsh
+++ oh-my-zsh/custom/aliases.zsh
@@ -2,7 +2,7 @@
 # alias ll='ls -lhF --time-style=long-iso --color=auto'
 # alias la='ls -lhAF --time-style=long-iso --color=auto'
 alias tree="tree -C"
-alias goproxy="export http_proxy=http://127.0.0.1:7890 https_proxy=http://127.0.0.1:7890 all_proxy=socks5://127.0.0.1:7891"
-alias noproxy="unset all_proxy http_proxy https_proxy"
+alias goproxy="export http_proxy=http://localhost:7890 https_proxy=http://localhost:7890 all_proxy=socks5://localhost:7890 no_proxy='localhost, 127.0.0.1'"
+alias noproxy="unset all_proxy http_proxy https_proxy no_proxy"
 
 type nvim >/dev/null 2>&1 && alias vim="nvim"
