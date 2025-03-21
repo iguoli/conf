4edc481aeaa2fb3a0c13692d385723945a7ce5f5
Guo,Li <lig@dxc.com>
2025-03-21 16:01:02 +0800

    Added back msedge alias

--- oh-my-zsh/custom/wsl.zsh
+++ oh-my-zsh/custom/wsl.zsh
@@ -1,2 +1,5 @@
-alias code='/mnt/c/Users/lguo5/AppData/Local/Programs/Microsoft\ VS\ Code/bin/code'
+alias code='"/mnt/c/Users/lguo5/AppData/Local/Programs/Microsoft VS Code/bin/code"'
+alias msedge='"/mnt/c/Program Files (x86)/Microsoft/Edge/Application/msedge.exe"'
+alias chromium-browser=msedge
+alias bcomp='"/mnt/c/Program Files/Beyond Compare 4/BComp.exe"'
 alias open='/mnt/c/Windows/explorer.exe'
