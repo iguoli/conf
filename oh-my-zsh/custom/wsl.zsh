023370ceef98ba0157d3cc2c72d6e54589442f1b
Guo, Li <28505129+iguoli@users.noreply.github.com>
2025-03-25 16:18:44 +0800

    Enclosed the values of environment variables with ugly single quotes nested by double quotes

--- oh-my-zsh/custom/wsl.zsh
+++ oh-my-zsh/custom/wsl.zsh
@@ -3,6 +3,6 @@
 # For example: WSLENV=LOCALAPPDATA/p:ProgramFiles/p:ProgramFiles(x86)/p:SystemRoot/p
 export BROWSER="$ProgramFiles(x86)/Microsoft/Edge/Application/msedge.exe"
 
-alias code="$LOCALAPPDATA/Programs/Microsoft VS Code/bin/code"
-alias bcomp="$ProgramFiles/Beyond Compare 4/BComp.exe"
+alias code='"$LOCALAPPDATA/Programs/Microsoft VS Code/bin/code"'
+alias bcomp='"$ProgramFiles/Beyond Compare 4/BComp.exe"'
 alias open="$SystemRoot/explorer.exe"
