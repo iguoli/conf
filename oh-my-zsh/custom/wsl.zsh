a123517c635a45568113ae70751ead2a23e80918
Guo, Li <28505129+iguoli@users.noreply.github.com>
2025-03-27 10:25:11 +0800

    Added WINIP to wsl.zsh

--- oh-my-zsh/custom/wsl.zsh
+++ oh-my-zsh/custom/wsl.zsh
@@ -1,7 +1,11 @@
-# Set environment variable WSLENV on the Windows to import windows environment variables
-# Use `ls env:` to find the environment varialbe names on Windows
+# First, you need to set an environment variable WSLENV on the Windows
 # For example: WSLENV=LOCALAPPDATA/p:ProgramFiles/p:ProgramFiles(x86)/p:SystemRoot/p
+# Use `ls env:` to find what environment variables are available on Windows
+# Then, WSL will import these variables into the WSL environment and convert them to Unix paths automatically
 export BROWSER="$ProgramFiles(x86)/Microsoft/Edge/Application/msedge.exe"
+
+# Set the Windows IP address
+export WINIP=$(ip route | awk '/default/ { print $3 }')
 
 alias code='"$LOCALAPPDATA/Programs/Microsoft VS Code/bin/code"'
 alias bcomp='"$ProgramFiles/Beyond Compare 4/BComp.exe"'
