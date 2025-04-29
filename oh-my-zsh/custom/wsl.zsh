04cd4923340475619560cb7451736cd6b80a6dc5
Guo,Li <lig@dxc.com>
2025-04-29 15:22:22 +0800

    Updated wsl.zsh

--- oh-my-zsh/custom/wsl.zsh
+++ oh-my-zsh/custom/wsl.zsh
@@ -2,11 +2,12 @@
 # For example: WSLENV=LOCALAPPDATA/p:ProgramFiles/p:ProgramFiles(x86)/p:SystemRoot/p
 # Use `ls env:` to find what environment variables are available on Windows
 # Then, WSL will import these variables into the WSL environment and convert them to Unix paths automatically
-export BROWSER="$ProgramFiles(x86)/Microsoft/Edge/Application/msedge.exe"
+# Add cursor, vscode, beyond compare and edge to PATH
+export PATH="$LOCALAPPDATA/Programs/cursor/resources/app/bin":"$LOCALAPPDATA/Programs/Microsoft VS Code/bin":"$ProgramFiles/Beyond Compare 4":"$ProgramFiles(x86)/Microsoft/Edge/Application":$PATH
 
 # Set the Windows IP address
 export WINIP=$(ip route | awk '/default/ { print $3 }')
 
-alias code='"$LOCALAPPDATA/Programs/Microsoft VS Code/bin/code"'
-alias bcomp='"$ProgramFiles/Beyond Compare 4/BComp.exe"'
+export BROWSER="msedge.exe"
+
 alias open="$SystemRoot/explorer.exe"
