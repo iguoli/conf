bfd2e6eeb0342ece2a0a6ed74957066e3cece200
Guo, Li <28505129+iguoli@users.noreply.github.com>
2025-06-09 09:51:22 +0800

    Updated aliases.zsh and wsl.zsh

--- oh-my-zsh/custom/aliases.zsh
+++ oh-my-zsh/custom/aliases.zsh
@@ -2,7 +2,11 @@
 # alias ll='ls -lhF --time-style=long-iso --color=auto'
 # alias la='ls -lhAF --time-style=long-iso --color=auto'
 alias tree="tree -C"
-alias goproxy="export http_proxy=http://localhost:7890 https_proxy=http://localhost:7890 all_proxy=socks5://localhost:7890 no_proxy='localhost, 127.0.0.1'"
-alias noproxy="unset all_proxy http_proxy https_proxy no_proxy"
+
+# Get the Windows host IP address
+export WINIP=$(ip route | awk '/default/ { print $3 }')
+
+alias goproxy="export http_proxy=http://$WINIP:7890 https_proxy=http://$WINIP:7890 all_proxy=socks5://$WINIP:7890 no_proxy='localhost, 127.0.0.1'"
+alias noproxy="unset all_proxy http_proxy https_proxy no_proxy HTTP_PROXY HTTPS_PROXY NO_PROXY WSL_PAC_URL"
 
 type nvim >/dev/null 2>&1 && alias vim="nvim"
--- oh-my-zsh/custom/wsl.zsh
+++ oh-my-zsh/custom/wsl.zsh
@@ -3,10 +3,7 @@
 # Use `ls env:` to find what environment variables are available on Windows
 # Then, WSL will import these variables into the WSL environment and convert them to Unix paths automatically
 # Add cursor, vscode, beyond compare and edge to PATH
-export PATH="$LOCALAPPDATA/Programs/Microsoft VS Code/bin":"$LOCALAPPDATA/Programs/Beyond Compare 5":"$ProgramFiles (x86)/Microsoft/Edge/Application":$PATH
-
-# Set the Windows IP address
-export WINIP=$(ip route | awk '/default/ { print $3 }')
+export PATH="$LOCALAPPDATA/Programs/Microsoft VS Code/bin":"$ProgramFiles (x86)/Microsoft/Edge/Application":$PATH
 
 export BROWSER="msedge.exe"
 
