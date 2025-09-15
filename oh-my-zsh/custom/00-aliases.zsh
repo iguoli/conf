c89bbc34afe8de8fc1cbfdeb11c33f525690c379
Guo, Li <28505129+iguoli@users.noreply.github.com>
2025-09-15 11:52:45 +0800

    Moved the WINIP settings to the wsl.zsh

--- oh-my-zsh/custom/00-aliases.zsh
+++ oh-my-zsh/custom/00-aliases.zsh
@@ -3,10 +3,4 @@
 # alias la='ls -lhAF --time-style=long-iso --color=auto'
 alias tree="tree -C"
 
-# Get the Windows host IP address
-export WINIP=$(ip route | awk '/default/ { print $3 }')
-
-alias goproxy="export http_proxy=http://$WINIP:7890 https_proxy=http://$WINIP:7890 all_proxy=socks5://$WINIP:7890 no_proxy='localhost, 127.0.0.1'"
-alias noproxy="unset all_proxy http_proxy https_proxy no_proxy HTTP_PROXY HTTPS_PROXY NO_PROXY WSL_PAC_URL"
-
 type nvim >/dev/null 2>&1 && alias vim="nvim"
