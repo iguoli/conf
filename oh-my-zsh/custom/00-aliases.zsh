ef663629566cf665b51528db93bc56447cc489ce
Guo, Li <28505129+iguoli@users.noreply.github.com>
2025-10-28 11:23:13 +0800

    Updated customized script files forthe omz

--- oh-my-zsh/custom/00-aliases.zsh
+++ oh-my-zsh/custom/00-aliases.zsh
@@ -1,6 +1,23 @@
 # alias l='ls -lhF --time-style=long-iso --color=auto'
 # alias ll='ls -lhF --time-style=long-iso --color=auto'
 # alias la='ls -lhAF --time-style=long-iso --color=auto'
+#
+alias goproxy="export \
+	http_proxy=http://$WINIP:7890 \
+	https_proxy=http://$WINIP:7890 \
+	all_proxy=socks5://$WINIP:7890 \
+	no_proxy='localhost, 127.0.0.1'"
+
+alias noproxy="unset \
+	all_proxy \
+	http_proxy \
+	https_proxy \
+	no_proxy \
+	ALL_PROXY \
+	HTTP_PROXY \
+	HTTPS_PROXY \
+	NO_PROXY"
+
 alias tree="tree -C"
 
 type nvim >/dev/null 2>&1 && alias vim="nvim"
--- oh-my-zsh/custom/wsl.zsh
+++ oh-my-zsh/custom/wsl.zsh
@@ -8,9 +8,6 @@ export PATH="$LOCALAPPDATA/Programs/Microsoft VS Code/bin":"$ProgramFiles (x86)/
 # Get the Windows host IP address
 export WINIP=$(ip route | awk '/default/ { print $3 }')
 
-alias goproxy="export http_proxy=http://$WINIP:7890 https_proxy=http://$WINIP:7890 all_proxy=socks5://$WINIP:7890 no_proxy='localhost, 127.0.0.1'"
-alias noproxy="unset all_proxy http_proxy https_proxy no_proxy HTTP_PROXY HTTPS_PROXY NO_PROXY WSL_PAC_URL"
-
 export BROWSER="msedge.exe"
 
 alias open="$SystemRoot/explorer.exe"
