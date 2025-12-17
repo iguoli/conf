362a42f3606d2baa6f65a6de01e0c053d7e9e420
Guo, Li <28505129+iguoli@users.noreply.github.com>
2025-12-17 11:16:15 +0800

    Updated sslcert.zsh

--- oh-my-zsh/custom/sslcert.zsh
+++ oh-my-zsh/custom/sslcert.zsh
@@ -1,13 +1,19 @@
 # Print subject, dates info of x509 certification.
-printcert () {
+print_certfile () {
     cmd="openssl x509 -noout -in $1 -subject -dates -ext subjectAltName";
     printf "\n%s\n\n" "${cmd}";
     eval "${cmd}"
 }
 
 # Print subject, expiration date info of multiple x509 certifications in a single file.
-print_certchain () {
+print_certchainfile () {
     cmd="openssl crl2pkcs7 -nocrl -certfile $1 | openssl pkcs7 -print_certs -noout -text | grep -P '(Subject|Not Before|Not After\s|DNS):'"
     printf "\n%s\n\n" "${cmd}";
     eval "${cmd}"
+}
+
+print_https_cert () {
+  cmd="echo | openssl s_client -connect $1:443 2>/dev/null | openssl x509 -noout -issuer -subject -dates -ext subjectAltName"
+  printf "\n%s\n\n" "${cmd}"
+  eval "${cmd}"
 }
